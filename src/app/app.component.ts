07b5aadd1355424ebc5746cbbc830dad248d9262
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-02-27 23:28:35 +0200

    feat-api: fetch api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './components/product/product.component';
 import { IProduct } from './models/products';
-import { products as data } from './data/products';
+import { ProductsService } from './services/products.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HttpClientModule],
+  providers: [ProductsService],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular shop';
+  products: IProduct[] = [];
 
-  products: IProduct[] = data;
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+    });
+  }
 }
