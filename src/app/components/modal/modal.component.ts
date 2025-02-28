b008ef0033bfccb449352d48e8b00280f06733ab
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-02-28 03:30:21 +0200

    feat-add-product: add button, add logic

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-modal',
@@ -11,5 +12,10 @@ export class ModalComponent {
 
   @Input()
   title!: string;
+
+  constructor(public modalService: ModalService) { }
+
+  ngOnInit(): void {
+  }
 
 }
