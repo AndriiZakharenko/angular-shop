69e266cec6aa3d4aeeaab35d06024ad8377b9612
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-02-28 02:35:10 +0200

    feat-create: modal create new product

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-product',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-product.component.html',
-  styleUrl: './create-product.component.scss'
+  styleUrl: './create-product.component.scss',
 })
 export class CreateProductComponent {
+[x: string]: any;
+  constructor() {}
 
+  form = new FormGroup({
+    title: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  get title() {
+    return this.form.controls.title as FormControl
+  }
+
+  ngOnInit(): void {}
+
+  submit() {
+    if (this.form.valid) {
+      console.log('Product Created:', this.form.value);
+    }
+  }
 }
